247da3b2a7841c31cbf41b1f20b01c6f24ba2ef8
Richardw2019 <rgarci3@clemson.edu>
2023-02-07 16:47:19 -0500

    Commit for assignment 2

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel, Field, validator
 from pydantic.typing import Literal
-from uuid import UUID, uuid4
 from datetime import date
 import json
 import os
-import random
+
 
 
 app = FastAPI()
@@ -15,21 +14,21 @@ app = FastAPI()
 class HumanName (BaseModel):
     text:str = Field(description = "Input the first and last name of the patient")
 
-    # @validator('text')
-    # def name_must_contain_space(cls, v):
-    #     if ' ' not in v:
-    #         raise ValueError('must contain first and last name with a space')
-    #     return v.title()
+    @validator('text')
+    def name_must_contain_space(cls, v):
+        if ' ' not in v:
+            raise ValueError('must contain first and last name with a space')
+        return v.title()
 
 class ContactPoint (BaseModel):
     system: str = 'phone'
     value: str
 
-    # @validator('value')
-    # def full_phone_number(cls, v):
-    #     if len(v) < 10:
-    #         raise ValueError('not a full phone number')
-    #     return v.title()
+    @validator('value')
+    def full_phone_number(cls, v):
+        if len(v) < 10:
+            raise ValueError('not a full phone number')
+        return v.title()
 
 class Address (BaseModel):
     line: str
@@ -39,86 +38,85 @@ class Address (BaseModel):
     postalCode: str
     country: str
 
-    # @validator('line')
-    # def line_check(cls, v):
-    #     if 'string' in v:
-    #         raise ValueError('not a full address')
-    #     return v.title()
+    @validator('line')
+    def line_check(cls, v):
+        if 'string' in v:
+            raise ValueError('not a full address')
+        return v.title()
     
-    # @validator('city',allow_reuse=True)
-    # def city_check(cls, v):
-    #     if 'string' in v:
-    #         raise ValueError('not a full address')
-    #     return v.title()
+    @validator('city',allow_reuse=True)
+    def city_check(cls, v):
+        if 'string' in v:
+            raise ValueError('not a full address')
+        return v.title()
     
-    # @validator('district',allow_reuse=True)
-    # def district_check(cls, v):
-    #     if 'string' in v:
-    #         raise ValueError('not a full address')
-    #     return v.title()
+    @validator('district',allow_reuse=True)
+    def district_check(cls, v):
+        if 'string' in v:
+            raise ValueError('not a full address')
+        return v.title()
 
-    # @validator('state',allow_reuse=True)
-    # def state_check(cls, v):
-    #     if 'string' in v:
-    #         raise ValueError('not a full address')
-    #     return v.title()
+    @validator('state',allow_reuse=True)
+    def state_check(cls, v):
+        if 'string' in v:
+            raise ValueError('not a full address')
+        return v.title()
 
-    # @validator('postalCode',allow_reuse=True)
-    # def postalCode_check(cls, v):
-    #     if 'string' in v:
-    #         raise ValueError('not a full address')
-    #     return v.title()
+    @validator('postalCode',allow_reuse=True)
+    def postalCode_check(cls, v):
+        if 'string' in v:
+            raise ValueError('not a full address')
+        return v.title()
     
-    # @validator('country',allow_reuse=True)
-    # def country_check(cls, v):
-    #     if 'string' in v:
-    #         raise ValueError('not a full address')
-    #     return v.title()
+    @validator('country',allow_reuse=True)
+    def country_check(cls, v):
+        if 'string' in v:
+            raise ValueError('not a full address')
+        return v.title()
 
 class Contact(BaseModel):
     relationship:str
     name: HumanName
     telecom:ContactPoint
     address: Address
-    gender: Literal['string','male', 'female','other', 'unknown']
+    gender: Literal['string','male', 'female','other', 'unknown', 'Male', "Female", "Other", "Unknown"]
 
 
-    # @validator('gender',allow_reuse=True)
-    # def gender_check(cls, v):
-    #     if 'string' in v:
-    #         raise ValueError('cannot leave string as gender')
-    #     return v.title()
+    @validator('gender',allow_reuse=True)
+    def contact_gender_check(cls, v):
+        if 'string' in v:
+            raise ValueError('cannot leave string as gender')
+        return v.title()
 
 
    
-    
-
 
 class Patient(BaseModel):
     resourceType: str = "Patient"
     active: bool = True
-    id: int
+    identifier: int = 1
     name: HumanName
     telecom: ContactPoint
-    gender: Literal['string','male', 'female','other', 'unknown']
-    birthdate: Union[date,str] = 'YYYY-MM-DD'
+    gender: Literal['string','male', 'female','other', 'unknown', 'Male', "Female", "Other", "Unknown"]
+    birthDate: str = 'YYYY-MM-DD'
     deceasedBoolean: bool = False
     address: Address
+    multipleBirthBoolean: bool = False
     contact: Contact
 
 
-    # @validator('gender',allow_reuse=True)
-    # def gender_check(cls, v):
-    #     if 'string' in v:
-    #         raise ValueError('cannot leave string as gender')
-    #     return v.title()
+    @validator('gender')
+    def patient_gender_check(cls, v):
+        if 'string' in v:
+            raise ValueError('cannot leave string as gender')
+        return v.title()
 
 
 
 
 
 @app.post("/createPatient")
-def post_info(patient: Patient):
+async def post_info(patient: Patient):
 
     patient_db = {}
 
@@ -131,6 +129,8 @@ def post_info(patient: Patient):
 
             #write to file, prettifying the json
             outfile.write(json.dumps(patient_db, indent=4))
+
+            return patient
     
     else:
         with open("patient.json", "r+") as outfile:
@@ -146,7 +146,6 @@ def post_info(patient: Patient):
 
             #create a new key-value pair in the existing dict
             db[str(key)] = patient.dict()
-            print(db)
 
             # #delete old dict
             # outfile.truncate(0)
@@ -155,8 +154,10 @@ def post_info(patient: Patient):
 
             #write to file, prettifying the json
             outfile.write(json.dumps(db, indent=4))
-            
 
+            return patient
+
+            
         
 @app.put("/updateAnyPatient/{patient_id}", response_model=Patient)
 async def update_patient_info(patient_id: str, patient:Patient):
@@ -180,7 +181,7 @@ async def update_patient_info(patient_id: str, patient:Patient):
             return update_patient_encoded
 
  
-@app.get("/retrievePatient/{patient_id}", response_model=Patient)
+@app.get("/retrievePatientInfo/{patient_id}", response_model=Patient)
 async def read_item(patient_id: str):
     #open file
     with open("patient.json", "r+") as outfile:
